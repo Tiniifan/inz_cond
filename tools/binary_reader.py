2590037376a6fae98a2bacc3dad7aa0769ba1788
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2025-10-15 22:28:04 +0200

    Better binary reader and binary writer
    Improves the binary reader and binary writer classes to support little-endian reading/writing. Also adds properties for easier access to data such as (length, current offset, order used).

--- tools/binary_reader.py
+++ tools/binary_reader.py
@@ -1,41 +1,57 @@
 import struct
 
 class BinaryDataReader:
-    def __init__(self, data):
+    def __init__(self, data, order='big'):
         self.data = data
-        self.offset = 0
-
+        self._offset = 0
+        self._order = order
+    
+    @property
+    def length(self):
+        return len(self.data)
+    
+    @property
+    def offset(self):
+        return self._offset
+    
+    @property
+    def order(self):
+        return self._order
+    
+    @order.setter
+    def order(self, value):
+        if value not in ('big', 'little'):
+            raise ValueError("Order must be 'big' or 'little'")
+        self._order = value
+    
     def read_bytes(self, length):
-        if self.offset + length > len(self.data):
+        if self._offset + length > len(self.data):
             raise ValueError("Attempt to read beyond the end of the buffer.")
-        chunk = self.data[self.offset:self.offset + length]
-        self.offset += length
+        chunk = self.data[self._offset:self._offset + length]
+        self._offset += length
         return chunk
-
+    
     def read_byte(self):
         return self.read_bytes(1)[0]
-
+    
     def read_byte_as_hex(self):
         return f"{self.read_bytes(1)[0]:02X}"
-
-    def read_int32(self):
-        return struct.unpack(">I", self.read_bytes(4))[0]
-
-    def read_int32_as_hex(self):
-        return struct.unpack(">I", self.read_bytes(4))[0]
-
-    def read_hex3(self):
-        return int.from_bytes(self.read_bytes(3), byteorder='big')
-
+    
+    def read_int32(self, order=None):
+        byte_order = order if order else self._order
+        format_char = ">I" if byte_order == 'big' else "<I"
+        return struct.unpack(format_char, self.read_bytes(4))[0]
+    
+    def read_int24(self, order=None):
+        byte_order = order if order else self._order
+        return int.from_bytes(self.read_bytes(3), byteorder=byte_order)
+    
     def skip(self, length):
-        if self.offset + length > len(self.data):
+        if self._offset + length > len(self.data):
             raise ValueError("Attempt to skip beyond the end of the buffer.")
-        self.offset += length
-
+        self._offset += length
+    
     def to_seek(self, position):
         if position < 0 or position > len(self.data):
             raise ValueError("Invalid seek position.")
-        self.offset = position
-
-    def get_position(self):
-        return self.offset
+        self._offset = position
--- tools/binary_writer.py
+++ tools/binary_writer.py
@@ -1,44 +1,64 @@
 import struct
 
 class BinaryDataWriter:
-    def __init__(self):
-        self.data = bytearray()
-        self.offset = 0
-
+    def __init__(self, order='big'):
+        self._data = bytearray()
+        self._offset = 0
+        self._order = order
+    
+    @property
+    def length(self):
+        return len(self._data)
+    
+    @property
+    def offset(self):
+        return self._offset
+    
+    @property
+    def data(self):
+        return bytes(self._data)
+    
+    @property
+    def order(self):
+        return self._order
+    
+    @order.setter
+    def order(self, value):
+        if value not in ('big', 'little'):
+            raise ValueError("Order must be 'big' or 'little'")
+        self._order = value
+    
     def write_bytes(self, b):
-        end_offset = self.offset + len(b)
-        if end_offset > len(self.data):
-            self.data.extend(b'\x00' * (end_offset - len(self.data)))
-        self.data[self.offset:end_offset] = b
-        self.offset = end_offset
-
+        end_offset = self._offset + len(b)
+        if end_offset > len(self._data):
+            self._data.extend(b'\x00' * (end_offset - len(self._data)))
+        self._data[self._offset:end_offset] = b
+        self._offset = end_offset
+    
     def write_byte(self, value):
         if not 0 <= value <= 0xFF:
             raise ValueError("Byte value must be in range 0-255.")
         self.write_bytes(bytes([value]))
-
-    def write_int32(self, value):
+    
+    def write_int32(self, value, order=None):
         if not 0 <= value <= 0xFFFFFFFF:
             raise ValueError("Int32 value must be in range 0-4294967295.")
-        self.write_bytes(struct.pack(">I", value))
-
-    def write_hex3(self, value):
+        byte_order = order if order else self._order
+        format_char = ">I" if byte_order == 'big' else "<I"
+        self.write_bytes(struct.pack(format_char, value))
+    
+    def write_int24(self, value, order=None):
         if not 0 <= value <= 0xFFFFFF:
             raise ValueError("Hex3 value must be in range 0-16777215.")
-        self.write_bytes(value.to_bytes(3, byteorder='big'))
-
+        byte_order = order if order else self._order
+        self.write_bytes(value.to_bytes(3, byteorder=byte_order))
+    
     def skip(self, length):
-        if self.offset + length > len(self.data):
-            self.data.extend(b'\x00' * (self.offset + length - len(self.data)))
-        self.offset += length
-
+        if self._offset + length > len(self._data):
+            self._data.extend(b'\x00' * (self._offset + length - len(self._data)))
+        self._offset += length
+    
     def to_seek(self, position):
-        if position < 0 or position > len(self.data):
+        if position < 0 or position > len(self._data):
             raise ValueError("Invalid seek position.")
-        self.offset = position
-
-    def get_position(self):
-        return self.offset
-
-    def get_data(self):
-        return bytes(self.data)
+        self._offset = position
